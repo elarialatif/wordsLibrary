c151ab0afb2f70b7b2f3a8c722a63fc15d2f9945
Ahmed Atef Fathy Mogeth <ahmed.atef@almotahidaeducation.com>
2019-03-11 09:42:30 +0200

    edit view article details

--- app/Http/Controllers/superAdmin/SuperAdminController.php
+++ app/Http/Controllers/superAdmin/SuperAdminController.php
@@ -4,9 +4,13 @@ namespace App\Http\Controllers\superAdmin;
 
 use App\Helper\UsersTypes;
 use App\Models\Article;
+use App\Models\Link;
+use App\Models\Sound;
 use App\Models\UserRate;
+use App\Models\Vocab;
 use App\Repository\ArticalRepository;
 use App\Repository\ContentListsRepository;
+use App\Repository\LinksRepository;
 use App\Repository\QuestionsRepository;
 use App\Repository\SoundsRepository;
 use App\Repository\UsersRepository;
@@ -23,10 +27,14 @@ class SuperAdminController extends Controller
         if ($list == null) {
             return redirect()->back()->with('error', 'الموضوع  غير موجود');
         }
-        $sound = SoundsRepository::findWhere('article_id', $article_id);
-        $questions = QuestionsRepository::findWhere('artical_id', $article_id);
-
-        return view('superadmin.viewArticleDetail', compact('article', 'sound', 'questions', 'list'));
+        $questionType=new Article();
+        $sound =Sound::where(['article_id'=>$article_id,'type'=>$questionType->getNormalArticleValue()])->first();
+        $soundStretch =Sound::where(['article_id'=>$article_id,'type'=>$questionType->getStretchArticleValue()])->first();
+        $questions=\App\Models\Question::where(['artical_id'=>$article_id,'type'=>$questionType->getNormalArticleValue()])->get();
+        $questionStretch=\App\Models\Question::where(['artical_id'=>$article_id,'type'=>$questionType->getStretchArticleValue()])->get();
+        $vocab=Vocab::where(['list_id'=>$article->list_id,'level'=>$article->level])->get();
+        $links=Link::where('list_id',$list->id)->get();
+        return view('superadmin.viewArticleDetail', compact('links','vocab','questionStretch','soundStretch','article', 'sound', 'questions', 'list'));
 
     }
 
